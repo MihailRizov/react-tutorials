d6f58c611c0d4c62d0b80c8ead3d5a8925373672
Mihail Rizov <mbrizov@gmail.com>
2022-10-01 20:27:41 +0300

    [fix] Unit test for expense components

--- apps/expense-tracker/src/app/components/expense-date/expense-date/expense-date.spec.tsx
+++ apps/expense-tracker/src/app/components/expense-date/expense-date/expense-date.spec.tsx
@@ -3,8 +3,11 @@ import { render } from '@testing-library/react';
 import ExpenseDate from './expense-date';
 
 describe('ExpenseDate', () => {
+  const mockDate = {
+    date: new Date(2020, 7, 14),
+  };
   it('should render successfully', () => {
-    const { baseElement } = render(<ExpenseDate />);
+    const { baseElement } = render(<ExpenseDate {...mockDate} />);
     expect(baseElement).toBeTruthy();
   });
 });
--- apps/expense-tracker/src/app/components/expense-item/expense-item/expense-item.spec.tsx
+++ apps/expense-tracker/src/app/components/expense-item/expense-item/expense-item.spec.tsx
@@ -3,8 +3,14 @@ import { render } from '@testing-library/react';
 import ExpenseItem from './expense-item';
 
 describe('ExpenseItem', () => {
+  const mockExpenseItem = {
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  };
+
   it('should render successfully', () => {
-    const { baseElement } = render(<ExpenseItem />);
+    const { baseElement } = render(<ExpenseItem {...mockExpenseItem} />);
     expect(baseElement).toBeTruthy();
   });
 });

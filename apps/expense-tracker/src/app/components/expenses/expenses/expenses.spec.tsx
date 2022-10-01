3ffbc3757e2abec1e1c317fac5703be0372b66cd
MihailRizov <46921657+MihailRizov@users.noreply.github.com>
2022-10-01 20:28:49 +0300

    [fix] Unit test for expense components (#5)

--- apps/expense-tracker/src/app/components/expenses/expenses/expenses.spec.tsx
+++ apps/expense-tracker/src/app/components/expenses/expenses/expenses.spec.tsx
@@ -3,8 +3,16 @@ import { render } from '@testing-library/react';
 import Expenses from './expenses';
 
 describe('Expenses', () => {
+  const mockExpenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+  ];
   it('should render successfully', () => {
-    const { baseElement } = render(<Expenses />);
+    const { baseElement } = render(<Expenses expenses={mockExpenses} />);
     expect(baseElement).toBeTruthy();
   });
 });

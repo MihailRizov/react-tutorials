4aff9a6d2893e995738d63f74b38baf5e2e776f5
MihailRizov <46921657+MihailRizov@users.noreply.github.com>
2022-10-24 19:43:27 +0300

    [feat] Add logic to read user input (#13)

--- apps/expense-tracker/src/app/components/new-expense/components/expense-form/expense-form.tsx
+++ apps/expense-tracker/src/app/components/new-expense/components/expense-form/expense-form.tsx
@@ -1,23 +1,82 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './expense-form.module.scss';
 
 /* eslint-disable-next-line */
 export interface ExpenseFormProps {}
 
 const ExpenseForm = (props: ExpenseFormProps) => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState(0);
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: '',
+  //   enteredAmount: 0,
+  //   enteredDate: '',
+  // });
+
+  const titleChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnteredTitle(event.target.value.trim());
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value.trim(),
+    // });
+    // setUserInput((prevState) => ({
+    //   ...prevState,
+    //   enteredTitle: event.target.value.trim(),
+    // }));
+  };
+
+  const amountChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnteredAmount(+event.target.value);
+    // setUserInput((prevState) => ({
+    //   ...prevState,
+    //   enteredTitle: event.target.value.trim(),
+    // }));
+  };
+
+  const dateChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setEnteredDate(event.target.value);
+    // setUserInput((prevState) => ({
+    //   ...prevState,
+    //   enteredTitle: event.target.value.trim(),
+    // }));
+  };
+
+  const submitHandler = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className={styles['new-expense__controls']}>
         <div className={styles['new-expense__control']}>
           <label>Title</label>
-          <input type='text' />
+          <input type='text' onChange={titleChangeHandler} />
         </div>
         <div className={styles['new-expense__control']}>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className={styles['new-expense__control']}>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2025-12-31' />
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2025-12-31'
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className={styles['new-expense__actions']}>

9362c3724217e43ac000da4412b5e0ecb3c85149
Mihail Rizov <mbrizov@gmail.com>
2022-11-16 17:34:11 +0200

    [feat] Add style to the input form

--- apps/react-style-tutorial/src/app/components/course-goals/course-input/course-input.tsx
+++ apps/react-style-tutorial/src/app/components/course-goals/course-input/course-input.tsx
@@ -9,18 +9,34 @@ export interface CourseInputProps {
 const CourseInput = (props: CourseInputProps) => {
   const { onAddGoal } = props;
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    setEnteredValue(event.target.value);
+    const inputValue = event.target.value.trim();
+
+    if (inputValue) {
+      setIsValid(true);
+    }
+
+    setEnteredValue(inputValue);
   };
 
   const formSubmitHandler = (event: FormEvent) => {
     event.preventDefault();
+    if (!enteredValue) {
+      setIsValid(false);
+      return;
+    }
+
     onAddGoal(enteredValue);
   };
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={styles['form-control']}>
+      <div
+        className={`${styles['form-control']} ${
+          !isValid && styles['invalid']
+        }`}>
         <label>Course Goal</label>
         <input type='text' onChange={goalInputChangeHandler} />
       </div>

75eeec41c9e9ca637d290a202747bcb2cae30d37
MihailRizov <46921657+MihailRizov@users.noreply.github.com>
2023-01-31 20:05:45 +0200

    [feat] Create error modal and users list components (#24)

--- apps/practice-project-add-user/src/app/app.tsx
+++ apps/practice-project-add-user/src/app/app.tsx
@@ -1,11 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from 'react';
 import styles from './app.module.scss';
-import AddUser from './components/users/add-user/add-user';
+import AddUser, { User } from './components/users/add-user/add-user';
+import UsersList from './components/users/users-list/users-list';
 
 export function App() {
+  const [userList, setUserList] = useState<User[]>([]);
+
+  const addUserHandler = (user: User): void => {
+    const userToBeAdded = { ...user, key: Date.now() };
+    setUserList((prevUserList) => [...prevUserList, userToBeAdded]);
+  };
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={userList} />
     </div>
   );
 }
--- apps/practice-project-add-user/src/app/components/users/add-user/add-user.tsx
+++ apps/practice-project-add-user/src/app/components/users/add-user/add-user.tsx
@@ -1,12 +1,22 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 import Button from '../../ui/button/button';
 import Card from '../../ui/card/card';
+import ErrorModal, { ErrorModalProps } from '../../ui/error-modal/error-modal';
 import styles from './add-user.module.scss';
 
-/* eslint-disable-next-line */
-export interface AddUserProps {}
+export interface AddUserProps {
+  onAddUser: (user: User) => void;
+}
+export interface User {
+  username: string;
+  age: number;
+  key?: number;
+}
 
 const AddUser = (props: AddUserProps) => {
+  const { onAddUser } = props;
+  const [error, setError] = useState<ErrorModalProps | null>();
+  const actionHandler = () => setError(null);
   const [enteredUsername, setEnteredUsername] = useState('');
   const userNameChangeHandler = (
     event: ChangeEvent<HTMLInputElement>
@@ -21,34 +31,53 @@ const AddUser = (props: AddUserProps) => {
 
   const addUserHandler = (event: FormEvent): void => {
     event.preventDefault();
-    if (!enteredUsername.trim().length || enteredAge <= 0) {
+    if (!enteredUsername.trim().length) {
+      setError({
+        title: 'Invalid username',
+        message: 'Please type a valid username',
+      });
       return;
     }
 
+    if (enteredAge <= 0) {
+      setError({ title: 'Invalid age', message: 'Please type a valid age' });
+      return;
+    }
+
+    onAddUser({ username: enteredUsername, age: enteredAge });
     setEnteredUsername('');
     setEnteredAge(0);
   };
 
   return (
-    <Card className={styles['input']}>
-      <form className={styles['']} onSubmit={addUserHandler}>
-        <label htmlFor='username'>Username</label>
-        <input
-          id='username'
-          type='text'
-          value={enteredUsername}
-          onChange={userNameChangeHandler}
+    <>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onAction={actionHandler}
         />
-        <label htmlFor='age'>Age (Years)</label>
-        <input
-          id='age'
-          type='number'
-          value={enteredAge || ''}
-          onChange={userAgeChangeHandler}
-        />
-        <Button type='submit'>Add user</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={styles['input']}>
+        <form className={styles['']} onSubmit={addUserHandler}>
+          <label htmlFor='username'>Username</label>
+          <input
+            id='username'
+            type='text'
+            value={enteredUsername}
+            onChange={userNameChangeHandler}
+          />
+          <label htmlFor='age'>Age (Years)</label>
+          <input
+            id='age'
+            type='number'
+            value={enteredAge || ''}
+            onChange={userAgeChangeHandler}
+          />
+          <Button type='submit'>Add user</Button>
+        </form>
+      </Card>
+    </>
   );
 };
 
